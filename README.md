1c1fff1f981725a96b37125efa3b4a9072bd969e
three <448987046@qq.com>
2018-12-13 16:58:47 +0800

    clear ci

--- README.md
+++
@@ -1,2 +0,0 @@
-# threeq.github.com
-出井的青蛙 http://blog.threeq.me
--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-host=$1
-
-echo 'start deploy ...'
-ssh -o "StrictHostKeyChecking no" $host 'rm -rf threeq.github.com && git clone https://github.com/threeq/threeq.github.com.git'
-
-echo 'startup ...'
-ssh -o "StrictHostKeyChecking no" $host 'cd threeq.github.com && bash scripts/startup.sh'
-
-echo 'end ----------------'
