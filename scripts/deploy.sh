9d1b2f3c3c6f30e2dcba04ea8823918d399be1ce
three <448987046@qq.com>
2018-12-13 16:04:08 +0800

    ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 host=$1
 
 echo 'start deploy ...'
-ssh $host 'rm -rf threeq.github.com && git clone https://github.com/threeq/threeq.github.com.git'
+ssh -o "StrictHostKeyChecking no" $host 'rm -rf threeq.github.com && git clone https://github.com/threeq/threeq.github.com.git'
 
 echo 'startup ...'
-ssh $host 'cd threeq.github.com && bash scripts/startup.sh'
+ssh -o "StrictHostKeyChecking no" $host 'cd threeq.github.com && bash scripts/startup.sh'
 
 echo 'end ----------------'
